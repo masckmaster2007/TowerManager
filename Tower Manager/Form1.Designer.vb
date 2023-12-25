df6ef6c167b70251acb2b948131cd8ab0daa7108
masckmaster2007 <63162857+masckmaster2007@users.noreply.github.com>
2023-12-25 09:57:24 +0100

    Ajoutez des fichiers projet.

---
+++ Tower Manager/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(353, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Pick Resource Folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(353, 65)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(79, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Pick Level"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(124, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(223, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(123, 67)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(224, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Lvl1", "Lvl2", "Lvl3", "Lvl4"})
+        Me.ComboBox1.Location = New System.Drawing.Point(123, 38)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "2.2 Resource Folder:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Tower Level:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Level Song:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 67)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Level TXT:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(123, 93)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(224, 20)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(353, 91)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(79, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Pick OGG"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(20, 116)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "Start"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(417, 126)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(65, 13)
+        Me.LinkLabel1.TabIndex = 12
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "By DimisAIO"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(356, 38)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(76, 13)
+        Me.LinkLabel2.TabIndex = 13
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Backup Folder"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(489, 151)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximumSize = New System.Drawing.Size(505, 190)
+        Me.MinimumSize = New System.Drawing.Size(505, 190)
+        Me.Name = "Form1"
+        Me.Text = "TowerManager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ Tower Manager/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Function FilePick(FileType As String, FileTypeDesc As String)
+        Dim Picker As New OpenFileDialog()
+        Picker.Filter = FileTypeDesc + "|*." + FileType
+        If Picker.ShowDialog() = DialogResult.OK Then
+            Return Picker.FileName
+        End If
+        Return False
+    End Function
+
+    Function FolderPick()
+        Dim Picker As New OpenFileDialog()
+        Picker.ValidateNames = False
+        Picker.CheckFileExists = False
+        Picker.CheckPathExists = True
+        Picker.FileName = "Resources"
+
+        If Picker.ShowDialog() = DialogResult.OK Then
+            Return Path.GetDirectoryName(Picker.FileName)
+        End If
+        Return False
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Folder = FolderPick()
+        If Not Directory.Exists(Path.Combine(Folder, "levels")) Then
+            MsgBox("This is not a valid Resources directory. Make sure that you use a 2.2 Resource directory!")
+            Return
+        End If
+        TextBox1.Text = Folder
+        Dim BackupFolder = Path.Combine(Folder, "bcp")
+        If Not Directory.Exists(BackupFolder) Then
+            Directory.CreateDirectory(BackupFolder)
+            My.Computer.FileSystem.CopyDirectory(Path.Combine(Folder, "levels"), Path.Combine(BackupFolder, "levels"), True)
+            My.Computer.FileSystem.CopyDirectory(Path.Combine(Folder, "songs"), Path.Combine(BackupFolder, "songs"), True)
+            MsgBox("A backup directory has been successfully created at" + Environment.NewLine + Environment.NewLine + BackupFolder)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Level = FilePick("*", "Raw or encoded Geometry Dash level")
+        TextBox2.Text = Level
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Level = FilePick("ogg", "OGG Song File")
+        TextBox3.Text = Level
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://dsc.gg/dimisaio")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim Lvl = ComboBox1.Text
+        Dim levels = Path.Combine(TextBox1.Text, "levels")
+        Dim songs = Path.Combine(TextBox1.Text, "songs")
+        Dim Lvl1File = Path.Combine(levels, "5001.txt")
+        Dim Lvl1Song = Path.Combine(songs, "10003039.ogg")
+
+        Dim Lvl2File = Path.Combine(levels, "5002.txt")
+        Dim Lvl2Song = Path.Combine(songs, "10003129.ogg")
+
+        Dim Lvl3File = Path.Combine(levels, "5003.txt")
+        Dim Lvl3Song = Path.Combine(songs, "10002875.ogg")
+
+        Dim Lvl4File = Path.Combine(levels, "5004.txt")
+        Dim Lvl4Song = Path.Combine(songs, "10006086.ogg")
+
+        Dim NewLevel = TextBox2.Text
+        Dim NewLevelSong = TextBox3.Text
+
+        If Lvl = "Lvl1" Then
+            File.Copy(NewLevel, Lvl1File, True)
+            File.Copy(NewLevelSong, Lvl1Song, True)
+        ElseIf Lvl = "Lvl2" Then
+            File.Copy(NewLevel, Lvl2File, True)
+            File.Copy(NewLevelSong, Lvl2Song, True)
+        ElseIf Lvl = "Lvl3" Then
+            File.Copy(NewLevel, Lvl3File, True)
+            File.Copy(NewLevelSong, Lvl3Song, True)
+        ElseIf Lvl = "Lvl4" Then
+            File.Copy(NewLevel, Lvl4File, True)
+            File.Copy(NewLevelSong, Lvl4Song, True)
+        End If
+        MsgBox("Done :D")
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("explorer.exe", Path.Combine(TextBox1.Text, "bcp"))
+    End Sub
+End Class
